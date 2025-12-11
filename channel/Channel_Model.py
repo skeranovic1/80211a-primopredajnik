73015ea667221b45485ae76cef9c4bb2c571f0ab
bsatara1 <91743322+bsatara1@users.noreply.github.com>
2025-12-11 23:43:39 +0100

    Kanal klasa

--- channel/Channel_Model.py
+++ channel/Channel_Model.py
@@ -1,28 +1,64 @@
 import numpy as np
 from Multipath import GetMultipathFilter
 from AWGN import Generate_AWGN
-import scipy as sc
+import scipy.signal as sc
 
-def Channel_Model(TX_Samples, Settings, Mode):
-    # 1. Transferring the mode parameters
-    Multipath_Select = Mode.Multipath  # 0/1 means exclude/include 
-    AW_GaussianNoise_Select = Mode.ThermalNoise
 
-    # 2. Transferring settings
-    N = Settings.NumberOfTaps  # For Multipath Modelpy
-    DelaySpread = Settings.DelaySpread
-    SampleRate = Settings.SampleRate
-    SNR_dB = Settings.SNR_dB  # Thermal Noise
+class Channel_Model:
+    """
+    OFDM Transmitter za IEEE 802.11a standard.
 
-    # 3. Generating multipath filterpp
-    FIR_Taps = GetMultipathFilter(SampleRate, DelaySpread, N)
-    if Multipath_Select == 1:
-        TX_Samples = sc.lfilter(FIR_Taps, 1, TX_Samples)
-    VarOutput = np.var(TX_Samples)
-    TX_Samples = TX_Samples / np.sqrt(VarOutput)
+    Atributi:
+    - NumberOfTaps
+    - DelaySpread
+    - SampleRate
+    - SNR_dB
+    """
 
-    # 4. Generating additive white gaussian noise (thermal noise)
-    if AW_GaussianNoise_Select == 1:
-        TX_Samples = TX_Samples + Generate_AWGN(TX_Samples, SNR_dB)
+    def __init__(self, settings, mode):
+        """
+        settings: objekt sa poljima:
+            - NumberOfTaps
+            - DelaySpread
+            - SampleRate
+            - SNR_dB
 
-    return TX_Samples, FIR_Taps
+        mode: objekt klase ChannelMode s poljima:
+            - Multipath (0/1)
+            - ThermalNoise (0/1)
+        """
+        self.settings = settings
+        self.mode = mode
+
+    def apply(self, tx_samples):
+        """
+        Primjenjuje model kanala na ulazne uzorke.
+        """
+
+        # 1. Mode parametri
+        multipath_select = self.mode.Multipath
+        awgn_select = self.mode.ThermalNoise
+
+        # 2. Settings parametri
+        N = self.settings.NumberOfTaps
+        delay_spread = self.settings.DelaySpread
+        sample_rate = self.settings.SampleRate
+        snr_db = self.settings.SNR_dB
+
+        # 3. Multipath FIR filter
+        fir_taps = GetMultipathFilter(sample_rate, delay_spread, N)
+
+        # Uključivanje multipath fadinga
+        if multipath_select == 1:
+            tx_samples = sc.lfilter(fir_taps, 1, tx_samples)
+
+        # Normalizacija energije
+        var_out = np.var(tx_samples)
+        if var_out > 0:
+            tx_samples = tx_samples / np.sqrt(var_out)
+
+        # 4. AWGN
+        if awgn_select == 1:
+            tx_samples = tx_samples + Generate_AWGN(tx_samples, snr_db)
+
+        return tx_samples, fir_taps
